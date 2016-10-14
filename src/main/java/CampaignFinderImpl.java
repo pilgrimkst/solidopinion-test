d602af8a653c75e7506feaa363b2894658684e45
Andrey Kuznetsov <andrey198701@gmail.com>
2016-10-14 11:58:35 +0300

    favors campaign impressions on same weights

--- src/main/java/CampaignFinderImpl.java
+++ src/main/java/CampaignFinderImpl.java
@@ -2,19 +2,27 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.*;
-import java.util.function.Function;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.IntStream;
 
 import static java.util.stream.Collectors.counting;
 import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.toList;
 
 public class CampaignFinderImpl implements CampaignFinder {
     private static final Logger LOGGER = LoggerFactory.getLogger(CampaignFinderImpl.class);
     private final Map<Integer, List<Integer>> segmentsToCampaigns;
     private final List<String> campaignNames;
+    private final List<AtomicLong> campaignImpressions;
 
     public CampaignFinderImpl(Map<Integer, List<Integer>> segmentsToCampaigns, List<String> campaignNames) {
         this.segmentsToCampaigns = segmentsToCampaigns;
         this.campaignNames = campaignNames;
+
+        campaignImpressions = IntStream
+                .range(0, campaignNames.size())
+                .mapToObj(i -> new AtomicLong(0))
+                .collect(toList());
     }
 
     @Override
@@ -31,13 +39,29 @@ public class CampaignFinderImpl implements CampaignFinder {
 
         LOGGER.debug("Campaigns results: {}", weighedCampaigns);
 
-        Optional<String> bestScoreCampaign = weighedCampaigns
+        Optional<Integer> bestScoreCampaign = weighedCampaigns
                 .entrySet()
                 .stream()
-                .max(Comparator.comparingLong(Map.Entry::getValue))
-                .map(e -> campaignNames.get(e.getKey()));
+                .max((o1, o2) -> {
+                    int compare = Long.compare(o1.getValue(), o2.getValue());
+                    if (compare == 0) {
+                        compare = -1 * Long.compare(
+                                campaignImpressions.get(o1.getKey()).get(),
+                                campaignImpressions.get(o2.getKey()).get());
+                    }
+                    return compare;
+                })
+                .map(Map.Entry::getKey);
+
+        bestScoreCampaign
+                .ifPresent(i -> {
+                    LOGGER.trace("Increasing impressions for campaign {}, impressions: {}", i, campaignImpressions);
+                    campaignImpressions.get(i).incrementAndGet();
+                });
+
         LOGGER.debug("Campaign with best score: {}", bestScoreCampaign);
 
-        return bestScoreCampaign;
+        return bestScoreCampaign.map(campaignNames::get);
     }
+
 }
--- src/test/groovy/CampaignFinderImplTest.groovy
+++ src/test/groovy/CampaignFinderImplTest.groovy
@@ -30,10 +30,37 @@ class CampaignFinderImplTest extends Specification {
         campaign                  | userSegments
         Optional.of("campaign_a") | [3, 4, 5, 10, 2, 200]
         Optional.of("campaign_a") | [3]
-        Optional.of("campaign_c") | [3]
         Optional.of("campaign_a") | [4, 10, 15]
         Optional.of("campaign_b") | [1024, 15, 200, 21, 9, 14, 15]
         Optional.empty()          | [9000, 29833, 65000]
+
+    }
+
+    def "find should return campaign with least impressions, if weights are same"() {
+        //        campaign_a 10 2
+        //        campaign_b 2 3
+        //        campaign_c 4 5
+
+        def campaigns = ["campaign_a", "campaign_b", "campaign_c"]
+        def segments = [2 : [0, 1],
+                        3 : [1],
+                        4 : [2],
+                        5 : [2],
+                        10: [0]]
+
+        def finder = new CampaignFinderImpl(segments, campaigns)
+
+        when:
+        def result = finder.find([2])
+
+        then:
+        result == Optional.of("campaign_a")
+
+        when:
+        result = finder.find([2])
+
+        then:
+        result == Optional.of("campaign_b")
 
     }
 }
